9591e465a51f06caca446d2661d1d6dbb6db4982
xiaojaja <17806251528@163.com>
2020-04-22 21:10:05 +0800

    first commit

---
+++ src/components/MenuBar.vue
@@ -0,0 +1,14 @@
+<template>
+<nav class="navbar navbar-default navbar-fixed-top">
+</template>
+
+
+<script lang="ts">
+import {Vue,Component} from "vue-property-decorator"
+
+@Component
+export default class MenuBar extends Vue{
+
+}
+
+</script>
